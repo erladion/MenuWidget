d5c20f7422e698fd680d6c82f62dcff891e9554a
erladion <johan.jansson94@gmail.com>
2023-02-18 11:23:55 +0100

    Working, but issues with hover indication

--- menuwidget.cpp
+++ menuwidget.cpp
@@ -1,5 +1,6 @@
 #include <QHBoxLayout>
 #include <QIcon>
+#include <QLabel>
 #include <QMap>
 #include <QPushButton>
 #include <QStackedWidget>
@@ -7,23 +8,138 @@
 
 #include "menuwidget.h"
 
-MenuItem::MenuItem(const QString& text, const QIcon& icon, MenuItem* parentItem, QWidget* parent)
-    : QWidget(parent), m_pTextLabel(new QPushButton(icon, text)), m_pParent(parentItem) {
-  QHBoxLayout* l = new QHBoxLayout();
+static const int ICON_SIZE = 16;
 
-  m_pRightIcon = new QPushButton();
+MenuItem::MenuItem(const QString &text, const QIcon &icon, MenuItem *parentItem,
+                   QWidget *parent)
+    : QWidget(parent), m_pTextLabel(new QLabel(text)), m_pParent(parentItem),
+      m_expanded(false), m_mouseOverExpander(false) {
 
+  QHBoxLayout *l = new QHBoxLayout();
+  l->setSpacing(5);
+  l->setContentsMargins(0, 0, 0, 0);
+  l->setAlignment(Qt::AlignLeft);
+
+  m_pLeftIcon = new QLabel();
+  m_pLeftIcon->setPixmap(icon.pixmap(ICON_SIZE, ICON_SIZE));
+
+  m_pTreeExpander = new TreeExpander();
+  m_pTreeExpander->setMinimumSize(QSize(ICON_SIZE, ICON_SIZE));
+  m_pTreeExpander->setMaximumSize(QSize(ICON_SIZE, ICON_SIZE));
+  m_pTreeExpander->hide();
+
+  connect(m_pTreeExpander, &TreeExpander::mouseEnter, this,
+          [this]() { m_mouseOverExpander = true; });
+
+  connect(m_pTreeExpander, &TreeExpander::mouseLeave, this,
+          [this]() { m_mouseOverExpander = false; });
+
+  l->addWidget(m_pLeftIcon);
   l->addWidget(m_pTextLabel);
-  l->addSpacerItem(new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Preferred));
-  l->addWidget(m_pRightIcon);
+  l->addSpacerItem(
+      new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Preferred));
+  l->addWidget(m_pTreeExpander);
+
+  setMinimumWidth(100);
 
   setLayout(l);
 }
 
-MenuWidget::MenuWidget(QWidget* parent)
-    : QWidget(parent), contents_(new QStackedWidget(this)), mainLayout_(new QVBoxLayout(this)), buttonLayout_(new QHBoxLayout) {
-  mainLayout_->addLayout(buttonLayout_);
-  mainLayout_->addWidget(contents_);
+void MenuItem::mousePressEvent(QMouseEvent *event) {
+  if (event->button() == Qt::LeftButton) {
+    if (m_mouseOverExpander && m_expanded) {
+      collapse();
+      emit itemClicked();
 
-  setLayout(mainLayout_);
+      return;
+    }
+
+    expand();
+    m_pTreeExpander->setExpanded(true);
+    emit itemClicked();
+  }
+}
+
+void MenuItem::expand() {
+  if (m_expanded) {
+    return;
+  }
+
+  for (MenuItem *child : m_children) {
+    child->show();
+  }
+  m_expanded = true;
+}
+void MenuItem::collapse() {
+  if (!m_expanded) {
+    return;
+  }
+
+  for (MenuItem *child : m_children) {
+    child->collapse();
+    child->hide();
+  }
+
+  m_pTreeExpander->setExpanded(false);
+  m_expanded = false;
+}
+
+void MenuItem::enterEvent(QEnterEvent *event) {
+  QWidget::enterEvent(event);
+
+  qWarning() << "Entering: " << m_pTextLabel->text();
+
+  QPalette palette = this->palette();
+  palette.setColor(QPalette::Window, QColor(173, 216, 230));
+  setPalette(palette);
+  repaint();
+}
+
+void MenuItem::leaveEvent(QEvent *event) {
+  QWidget::leaveEvent(event);
+
+  qWarning() << "Leaving: " << m_pTextLabel->text();
+
+  QPalette palette = this->palette();
+  palette.setColor(QPalette::Window, Qt::white);
+  setPalette(palette);
+  repaint();
+}
+
+MenuWidget::MenuWidget(QWidget *parent)
+    : QWidget(parent), m_pContents(new QStackedWidget(this)),
+      m_pMainLayout(new QHBoxLayout(this)), m_pButtonLayout(new QVBoxLayout) {
+  QFrame *f = new QFrame(this);
+  f->setFrameShape(QFrame::Box);
+  f->setLayout(m_pButtonLayout);
+  f->setMaximumWidth(200);
+  m_pButtonLayout->setContentsMargins(0, 0, 0, 0);
+
+  m_pMainLayout->addWidget(f);
+  m_pMainLayout->addWidget(m_pContents);
+
+  setLayout(m_pMainLayout);
+}
+
+MenuItem *MenuWidget::addItem(const QString &text, const QIcon &icon,
+                              QWidget *widget, MenuItem *parent) {
+  MenuItem *menu;
+  if (parent) {
+    menu = new MenuItem(text, icon, parent);
+    menu->setContentsMargins(parent->contentsMargins().left() + ICON_SIZE, 0, 0,
+                             0);
+
+    parent->addChild(menu);
+  } else {
+    menu = new MenuItem(text, icon);
+  }
+  connect(menu, &MenuItem::itemClicked, this, [this, menu]() {
+    m_pContents->setCurrentIndex(m_menuIndices.value(menu));
+  });
+
+  m_pButtonLayout->addWidget(menu);
+  int index = m_pContents->addWidget(widget);
+  m_menuIndices.insert(menu, index);
+
+  return menu;
 }
--- menuwidget.h
+++ menuwidget.h
@@ -1,40 +1,111 @@
 #include <QtWidgets>
 
+class TreeExpander : public QWidget {
+  Q_OBJECT
+public:
+  void setExpanded(bool expanded) {
+    m_expanded = expanded;
+    repaint();
+  }
+
+signals:
+  void mouseEnter();
+  void mouseLeave();
+
+protected:
+  void paintEvent(QPaintEvent *event) override {
+    QStyleOption opt;
+    opt.initFrom(this);
+
+    QPainter painter(this);
+    drawTreeExpander(&painter, &opt);
+  }
+
+  void enterEvent(QEnterEvent *event) override { emit mouseEnter(); }
+  void leaveEvent(QEvent *event) override { emit mouseLeave(); }
+
+private:
+  void drawTreeExpander(QPainter *painter, const QStyleOption *option) {
+    painter->setPen(QPen(option->palette.color(QPalette::ButtonText), 1,
+                         Qt::SolidLine, Qt::RoundCap, Qt::MiterJoin));
+    painter->setRenderHint(QPainter::Antialiasing, true);
+
+    int padding = option->rect.width() / 4;
+    int wHalf = option->rect.width() / 2;
+    int hHalf = option->rect.height() / 2;
+    int wPadded = option->rect.width() - padding;
+    int hPadded = option->rect.height() - padding;
+
+    QPoint p1, p2, p3;
+    if (!m_expanded) {
+      p1 = QPoint(wHalf, padding);
+      p2 = QPoint(wPadded, hHalf);
+      p3 = QPoint(wHalf, hPadded);
+    } else {
+      p1 = QPoint(padding, hHalf);
+      p2 = QPoint(wHalf, hPadded);
+      p3 = QPoint(wPadded, hHalf);
+    }
+    painter->drawLine(p1, p2);
+    painter->drawLine(p2, p3);
+  }
+
+  bool m_expanded = false;
+};
+
 class MenuItem : public QWidget {
   Q_OBJECT
- public:
-  MenuItem(const QString& text, const QIcon& icon, MenuItem* parentItem = nullptr, QWidget* parent = nullptr);
+public:
+  MenuItem(const QString &text, const QIcon &icon,
+           MenuItem *parentItem = nullptr, QWidget *parent = nullptr);
 
- private:
-  QPushButton* m_pTextLabel;
-  QPushButton* m_pRightIcon;
+  void addChild(MenuItem *child) {
+    m_children.append(child);
+    if (!m_expanded) {
+      child->hide();
+    }
+    m_pTreeExpander->show();
+  }
 
-  MenuItem* m_pParent;
+  QSize sizeHint() const override { return QSize(100, 25); }
+
+signals:
+  void itemClicked();
+
+protected:
+  void mousePressEvent(QMouseEvent *event) override;
+  void leaveEvent(QEvent *event) override;
+  void enterEvent(QEnterEvent *event) override;
+
+private:
+  void expand();
+  void collapse();
+
+  QLabel *m_pLeftIcon;
+  QLabel *m_pTextLabel;
+  TreeExpander *m_pTreeExpander;
+
+  MenuItem *m_pParent;
+  QList<MenuItem *> m_children;
+
+  bool m_expanded;
+  bool m_mouseOverExpander;
 };
 
 class MenuWidget : public QWidget {
   Q_OBJECT
 
- public:
-  explicit MenuWidget(QWidget* parent = nullptr);
+public:
+  explicit MenuWidget(QWidget *parent = nullptr);
 
-  QPushButton* addItem(const QString& text, const QIcon& icon = QIcon(), QWidget* widget = nullptr, QPushButton* parent = nullptr);
+  MenuItem *addItem(const QString &text, const QIcon &icon = QIcon(),
+                    QWidget *widget = nullptr, MenuItem *parent = nullptr);
 
-  void showWidget(QWidget* widget);
-  void showWidget(int index);
+  void addStretch() { m_pButtonLayout->addStretch(); }
 
- private:
-  QPushButton* createButton(const QString& text, const QIcon& icon = QIcon());
-  void showSubItems(QPushButton* button);
-  void hideSubItems(QPushButton* button);
-  void updateCurrentWidget();
-  void showParent(QPushButton* parent);
-
-  QStackedWidget* contents_;
-  QVBoxLayout* mainLayout_;
-  QHBoxLayout* buttonLayout_;
-  QList<QPushButton*> items_;
-  QList<QWidget*> widgets_;
-  QMap<QPushButton*, QList<QPushButton*>> subItems_;
-  QMap<QPushButton*, QWidget*> subWidgets_;
+private:
+  QStackedWidget *m_pContents;
+  QHBoxLayout *m_pMainLayout;
+  QVBoxLayout *m_pButtonLayout;
+  QMap<MenuItem *, int> m_menuIndices;
 };
